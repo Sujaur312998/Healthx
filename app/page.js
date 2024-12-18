d310004d4428af044f469d2915be8f7b34994b91
Sujaur312998 <suja312998@gmail.com>
2024-12-18 22:23:45 +0600

    update doctor

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Hero from "@/components/hero_section/Hero";
 import SearchBar from "@/components/search/SearchBar";
 import Services from "@/components/Services/Services";
 import Health_Services from "@/components/health_services/Health_Services";
-import Doctors from "@/components/doctors/Doctors";
 import Contact from "@/components/contact_form/Contact";
 import Footer from "@/components/footer/Footer";
+import Doctors from "@/components/doctors/doctors";
 
 export default function Home() {
   return (

e847d6555c5bc30cdda00ff721b6d58b2cb14faf
Sujaur312998 <suja312998@gmail.com>
2024-12-20 02:05:04 +0600

    update

--- app/page.js
+++ app/page.js
@@ -3,17 +3,9 @@ import Hero from "@/components/hero_section/Hero";
 import SearchBar from "@/components/search/SearchBar";
 import Services from "@/components/Services/Services";
 import Health_Services from "@/components/health_services/Health_Services";
-import Contact from "@/components/contact_form/Contact";
 import Doctors from "@/components/doctors/doctors";
+import Contact from "@/components/contact_form/Contact";
 import Footer from "@/components/footer/Footer";
-
-// const Hero = dynamic(() => import("@/components/hero_section/Hero"),{ssr:false})
-// const SearchBar = dynamic(() => import("@/components/search/SearchBar"),{ssr:false})
-// const Services = dynamic(() => import("@/components/Services/Services"),{ssr:false})
-// const Health_Services = dynamic(() =>import("@/components/health_services/Health_Services"),{ssr:false})
-// const Contact = dynamic(() => import("@/components/contact_form/Contact"),{ssr:false})
-// const Footer = dynamic(() => import("@/components/footer/Footer"),{ssr:false})
-// const Doctors = dynamic(() => import("@/components/doctors/doctors"),{ssr:false})
 
 export default function Home() {
   return (
@@ -22,10 +14,9 @@ export default function Home() {
       <SearchBar />
       <Services />
       <Health_Services />
-      {/* 
       <Doctors />
       <Contact />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
